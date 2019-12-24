55f9a374e6b505f7e6d3462f6f39e4df4711af66
Victor Wu <wujunhua1990@163.com>
2019-12-24 19:50:32 +0800

    Add IndexedDB-demo

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>indexedDB的基本使用</h1>
+    <div id="nav">
+      <router-link to="/">Base</router-link> |
+      <router-link to="/about">More</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+body{
+  padding: 20px !important;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Demo.vue
@@ -0,0 +1,417 @@
+<template>
+    <div>
+        <Tabs value="name1">
+            <TabPane label="建立连接" name="name1">
+                <div style="text-align:left">
+                    <div>
+                        <Button type="success" @click="doSetup"
+                            >建立连接</Button
+                        >
+                    </div>
+                    <div>
+                        <span>运行结果：{{ msg }}</span>
+                    </div>
+                    <div>
+                        <span>数据库名：{{ dbInfo.name }}</span>
+                        <br />
+                        <span>版本号：{{ dbInfo.version }}</span>
+                    </div>
+                </div>
+            </TabPane>
+            <TabPane label="创建表" name="name2">
+                <div style="text-align:left">
+                    <div>
+                        <Button type="success" @click="doCreateTable"
+                            >创建表</Button
+                        >
+                    </div>
+                    <div>
+                        <span>运行结果：{{ msg }}</span>
+                    </div>
+                    <div>
+                        <span>数据库名：{{ dbInfo.name }}</span>
+                        <br />
+                        <span>版本号：{{ dbInfo.version }}</span>
+                    </div>
+                </div>
+            </TabPane>
+            <TabPane label="添加数据" name="name3">
+                <div style="text-align:left">
+                    <div>
+                        <Button type="success" @click="doAddData"
+                            >添加数据</Button
+                        >
+                        <Button
+                            type="success"
+                            @click="doAddMultData"
+                            style="margin-left:30px"
+                            >添加批量数据</Button
+                        >
+                    </div>
+                    <div>
+                        <span>运行结果：{{ msg }}</span>
+                    </div>
+                </div>
+            </TabPane>
+            <TabPane label="添加索引查询数据" name="name4">
+                <div style="text-align:left">
+                    <div>
+                        <Button type="success" @click="doAddIndex"
+                            >添加索引</Button
+                        >
+                        <Button
+                            type="success"
+                            @click="doSearchDataFromIndex"
+                            style="margin-left:30px"
+                            >查询数据</Button
+                        >
+                    </div>
+                    <div>
+                        <span>运行结果：{{ msg }}</span>
+                    </div>
+                </div>
+            </TabPane>
+            <TabPane label="事务处理" name="name5">
+                <div style="text-align:left">
+                    <div>
+                        <Button type="success" @click="doTransaction"
+                            >事务示例</Button
+                        >
+                    </div>
+                    <div>
+                        <span>运行结果：{{ msg }}</span>
+                    </div>
+                </div>
+            </TabPane>
+        </Tabs>
+    </div>
+</template>
+
+<script>
+let db;
+export default {
+    data() {
+        return {
+            msg: '',
+            dbInfo: {
+                name: '',
+                version: ''
+            }
+        };
+    },
+    mounted() {},
+    methods: {
+        check() {
+            return new Promise((resolve, reject) => {
+                if (!window.indexedDB) {
+                    reject(new Error('该浏览器不支持indexedDB'));
+                } else {
+                    resolve();
+                }
+            });
+        },
+        open() {
+            return new Promise((resolve, reject) => {
+                let request = window.indexedDB.open('SchoolDatabase');
+                request.onerror = e => {
+                    reject(
+                        new Error(
+                            '创建数据库连接失败: ' + e.target.error.message
+                        )
+                    );
+                };
+                request.onsuccess = e => {
+                    // 创建数据库连接成功
+                    // 获得IDBDatabase对象
+                    let db = e.target.result;
+                    resolve(db);
+                };
+                request.onblocked = e => {
+                    reject(
+                        new Error(
+                            '创建数据库连接失败: ' + e.target.error.message
+                        )
+                    );
+                };
+            });
+        },
+        openAndCreateTable() {
+            return new Promise((resolve, reject) => {
+                let request = window.indexedDB.open(
+                    'SchoolDatabase',
+                    this.dbInfo.version + 1
+                );
+                request.onerror = e => {
+                    reject(new Error('创建表失败: ' + e.target.error.message));
+                };
+                request.onsuccess = e => {
+                    // 创建数据库连接成功
+                    // 获得IDBDatabase对象
+                    let db = e.target.result;
+                    resolve(db);
+                };
+                request.onupgradeneeded = e => {
+                    let db = e.target.result;
+                    // 创建使用id作为主键的student表
+                    db.createObjectStore('student', { keyPath: 'id' });
+                };
+                request.onblocked = e => {
+                    reject(
+                        new Error(
+                            '创建数据库连接失败: ' + e.target.error.message
+                        )
+                    );
+                };
+            });
+        },
+        openAndAddIndex() {
+            return new Promise((resolve, reject) => {
+                let request = window.indexedDB.open(
+                    'SchoolDatabase',
+                    this.dbInfo.version + 1
+                );
+                request.onerror = e => {
+                    reject(
+                        new Error('创建索引失败: ' + e.target.error.message)
+                    );
+                };
+                request.onsuccess = e => {
+                    // 创建数据库连接成功
+                    // 获得IDBDatabase对象
+                    let db = e.target.result;
+                    resolve(db);
+                };
+                request.onupgradeneeded = e => {
+                    let store = e.target.transaction.objectStore('student');
+                    store.createIndex('ageIndex', 'age', { unique: false });
+                };
+                request.onblocked = e => {
+                    reject(
+                        new Error('创建索引失败: ' + e.target.error.message)
+                    );
+                };
+            });
+        },
+        close() {
+            if (db) {
+                db.close();
+            }
+            db = null;
+        },
+        async setup() {
+            if (db) {
+                this.dbInfo = {
+                    name: db.name,
+                    version: db.version
+                };
+            } else {
+                await this.check();
+                db = await this.open();
+                this.dbInfo = {
+                    name: db.name,
+                    version: db.version
+                };
+            }
+        },
+        async createTable() {
+            if (!Array.from(db.objectStoreNames).includes('student')) {
+                this.close();
+                db = await this.openAndCreateTable();
+                this.dbInfo = {
+                    name: db.name,
+                    version: db.version
+                };
+            }
+        },
+        async createIndex() {
+            let store = db
+                .transaction('student', 'readonly')
+                .objectStore('student');
+            if (!Array.from(store.indexNames).includes('ageIndex')) {
+                this.close();
+                db = await this.openAndAddIndex();
+                this.dbInfo = {
+                    name: db.name,
+                    version: db.version
+                };
+            }
+        },
+        async addData(data) {
+            let addPromise = (objectStore, student) => {
+                return new Promise((resolve, reject) => {
+                    let request = objectStore.add(student);
+                    request.onsuccess = () => {
+                        resolve();
+                    };
+                    request.onerror = e => {
+                        reject(
+                            new Error('添加数据失败: ' + e.target.error.message)
+                        );
+                    };
+                });
+            };
+            let students = Array.isArray(data) ? data : [data];
+            let transaction = db.transaction('student', 'readwrite');
+            return new Promise((resolve, reject) => {
+                let objectStore = transaction.objectStore('student');
+                Promise.all(
+                    students.map(student => addPromise(objectStore, student))
+                )
+                    .then(() => {
+                        resolve();
+                    })
+                    .catch(e => {
+                        transaction.abort(e.message);
+                        reject(
+                            new Error(e.message)
+                        );
+                    });
+            });
+        },
+        async searchDataFromIndex() {
+            return new Promise((resolve, reject) => {
+                let transaction = db.transaction('student', 'readonly');
+                let objectStore = transaction.objectStore('student');
+                let dbIndex = objectStore.index('ageIndex');
+                let request = dbIndex.openCursor(IDBKeyRange.bound(0, 30));
+                let result = [];
+                request.onsuccess = e => {
+                    let cursor = e.target.result;
+                    if (cursor) {
+                        result.push(cursor.value);
+                        cursor.continue();
+                    } else {
+                        resolve(result);
+                    }
+                };
+                request.onerror = e => {
+                    reject(
+                        new Error('添加数据失败: ' + e.target.error.message)
+                    );
+                };
+            });
+        },
+        async doSetup() {
+            try {
+                this.setup();
+                this.msg = '创建数据库连接成功';
+            } catch (error) {
+                this.msg = error.message;
+            }
+        },
+        async doCreateTable() {
+            try {
+                await this.setup();
+                this.msg = '数据库连接成功, 正在初始化表';
+                await this.createTable();
+                this.msg = '初始化表成功';
+            } catch (error) {
+                this.msg = error.message;
+            }
+        },
+        async doAddData() {
+            try {
+                await this.setup();
+                this.msg = '数据库连接成功, 正在初始化表';
+                await this.createTable();
+                this.msg = '初始化表成功, 正在添加数据';
+                await this.addData({
+                    id: '0001',
+                    name: 'Victor',
+                    age: 29
+                });
+                this.msg = '添加数据成功';
+            } catch (error) {
+                this.msg = error.message;
+            }
+        },
+        async doAddMultData() {
+            let students = [
+                {
+                    id: '0002',
+                    name: 'Lucy',
+                    age: 15
+                },
+                {
+                    id: '0003',
+                    name: 'Mike',
+                    age: 35
+                },
+                {
+                    id: '0004',
+                    name: 'David',
+                    age: 28
+                }
+            ];
+            try {
+                await this.setup();
+                this.msg = '数据库连接成功, 正在初始化表';
+                await this.createTable();
+                this.msg = '初始化表成功, 正在添加数据';
+                await this.addData(students);
+                this.msg = '添加数据成功';
+            } catch (error) {
+                this.msg = error.message;
+            }
+        },
+        async doAddIndex() {
+            try {
+                await this.setup();
+                this.msg = '数据库连接成功, 正在初始化表';
+                await this.createTable();
+                this.msg = '初始化表成功, 正在添加索引';
+                await this.createIndex();
+                this.msg = '添加索引成功';
+            } catch (error) {
+                this.msg = error.message;
+            }
+        },
+        async doSearchDataFromIndex() {
+            try {
+                await this.setup();
+                this.msg = '数据库连接成功, 正在初始化表';
+                await this.createTable();
+                this.msg = '初始化表成功, 正在添加索引';
+                await this.createIndex();
+                this.msg = '添加索引成功，正在查询数据';
+                let students = await this.searchDataFromIndex();
+                this.msg =
+                    '查询数据成功，年龄在0-30之间的学生为: ' +
+                    students.map(student => student.name).join('|');
+            } catch (error) {
+                this.msg = error.message;
+            }
+        },
+        async doTransaction() {
+            let students = [
+                {
+                    id: '0005',
+                    name: 'Jay',
+                    age: 32
+                },
+                {
+                    id: '0006',
+                    name: 'Mar',
+                    age: 35
+                },
+                {
+                    id: '0004',
+                    name: 'David',
+                    age: 28
+                }
+            ];
+            try {
+                await this.setup();
+                this.msg = '数据库连接成功, 正在初始化表';
+                await this.createTable();
+                this.msg = '初始化表成功, 正在添加数据';
+                await this.addData(students);
+                this.msg = '添加数据成功';
+            } catch (error) {
+                this.msg = error.message;
+            }
+        }
+    }
+};
+</script>
+<style lang="scss" scoped></style>
